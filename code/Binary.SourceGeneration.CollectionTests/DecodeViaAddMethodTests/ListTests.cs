c1141d7b70040f6d1d02034018fbf9cfed2b1dd1
miko <stdarg@outlook.com>
2023-03-07 22:06:13 +0800

    Add empty unit test project for collection types

---
+++ code/Binary.SourceGeneration.CollectionTests/DecodeViaAddMethodTests/ListTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Mikodev.Binary.SourceGeneration.CollectionTests.DecodeViaAddMethodTests;
+
+using Mikodev.Binary.Attributes;
+using Xunit;
+
+[SourceGeneratorContext]
+public partial class ListSourceGeneratorContext { }
+
+public class ListTests
+{
+    [Fact(DisplayName = "Placeholder")]
+    public void Placeholder() { }
+}
